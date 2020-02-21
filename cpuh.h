9fb4f2180d491b92cc746d3c63ca66227b75f9ba
ScipioGothicus <prestobeans@gmail.com>
2020-02-21 09:15:22 -0700

    Update cpuh

--- cpuh.h
+++ cpuh.h
@@ -4,3 +4,11 @@
 #define SUB_FLAG (1 << 6);
 #define HALF_CARRY_FLAG (1 << 5);
 #define CARRY_FLAG (1 << 4);
+//D = data (ex. LD_BC_D is same as LD BC, d16)
+void NOP(void);
+void LD_BC_DD(unsigned short operand);
+void LD_BCP_A(void);
+void INC_BC(void);
+void INC_B(void);
+void DEC_B(void);
+void LD_B_
